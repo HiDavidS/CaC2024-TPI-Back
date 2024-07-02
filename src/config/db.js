8d61c0e648eff84ce526b209ede591c52f6579e7
GonMan22 <gonzalopf1@gmail.com>
2024-07-02 16:24:21 -0600

    Agrego datos de alwaydata

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'root',
-  database: 'tiendaropa',
-  port: 3306
+  host: 'mysql-tpcac2024.alwaysdata.net',
+  user: 'tpcac2024_db',
+  password: 'i6qPBW3RPdeHx2h',
+  database: 'tpcac2024_db',
+  port: 3306 ,
 });
 
 module.exports = pool.promise();
